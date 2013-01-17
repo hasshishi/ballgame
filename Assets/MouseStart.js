994574642eaa3ba67c6239ace904e220dcb9e944
hashishi <bcah1220050@gn.iwasaki.ac.jp>
2013-01-17 11:26:13 +0900

    ballgame
    ボールを板にあてるゲーム

---
+++ Assets/MouseStart.js
@@ -0,0 +1,8 @@
+function Start () { // 開始時は重力影響なし
+this.rigidbody.useGravity = false;
+}
+function Update () { // マウスクリックで重力開始
+if (Input.GetButtonDown("Fire1")) {
+this.rigidbody.useGravity = true;
+}
+}
